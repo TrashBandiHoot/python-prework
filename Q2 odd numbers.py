b7072167545e55dffc60c81cdf589ebc0ef2a097
Dominic Davenport <davenport.dominic@gmail.com>
2022-08-30 21:02:53 -0500

    updated comments for professor

--- Q2 odd numbers.py
+++ Q2 odd numbers.py
@@ -1,9 +1,10 @@
-
+# QUESTION 2
+# Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
 
 def first_odds():
     for x in range(1, 100):
         if x % 2 == 1:
             print(x)
-    return
+    
 
 first_odds()
